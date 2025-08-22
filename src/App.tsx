b6a1d6cc29a9fa419fc9fadcbab3e0507418e837
bibin francis <bibin.francis@cubettech.com>
2025-08-22 20:19:57 +0530

    asda

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import { AuthProvider } from './context/AuthContext';
+import { Suspense } from 'react';
 import AppRouter from './router';
+import { Toaster } from 'sonner';
+import { AuthProvider } from "./context/AuthContext.tsx";
 
 const App = () => {
   return (
-    <AuthProvider>
-      <AppRouter />
-    </AuthProvider>
+    <>
+    <Toaster richColors position="top-center" closeButton={true} />
+    <Suspense fallback={<div>Loading...</div>}>
+        <AuthProvider>
+
+        <AppRouter />
+          </AuthProvider>
+
+    </Suspense>
+  </>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { ThemeProvider } from "./context/ThemeContext.tsx";
+import { AuthProvider } from "./context/AuthContext.tsx";
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <StrictMode>
+    {/* <ThemeProvider> */}
+        {/* <AuthProvider> */}
+          <App />
+        {/* </AuthProvider> */}
+    {/* </ThemeProvider> */}
+  </StrictMode>,
 );
