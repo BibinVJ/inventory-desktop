58dcb593f0d7af115df878af6f80c40c561bbb39
bibin francis <bibin.francis@cubettech.com>
2025-08-27 10:58:25 +0530

    fix:cicd

--- electron/services/api.js
+++ electron/services/api.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const api = axios.create({
-  baseURL: process.env.API_BASE_URL,
+  baseURL: process.env.API_BASE_URL || 'http://localhost:3000/api',
 });
 
 module.exports = api;
